b0e51d8cfc60c4f427501107cbe99ba1fcf465d3
Srivatsa <srivatsa.sharma@keysight.com>
2018-12-05 23:00:47 +0530

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component, ElementRef, ViewChild, Input, OnInit } from '@angular/core';
 import { Message } from './message';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [trigger('highlightMessage', [
+    state('highlighted', style({
+      backgroundColor: 'lightgoldenrodyellow',
+    })),
+    state('normal', style({})),
+    transition('highlighted => normal', [
+      animate('2s')
+    ]),
+  ])]
 })
 export class AppComponent implements OnInit {
   title = 'app-selftalk';
@@ -13,8 +24,15 @@ export class AppComponent implements OnInit {
   public person2: string = "Bob";
   public inputText;
   public Math = Math;
+  public highlightLatestMessage;
   @ViewChild('InputText') public inputTextElementref: ElementRef;
 
+  public constructor() {
+    this.messages.push({ alice: 'Hi', bob: 'Hi there', isComplete: true });
+    this.messages.push({ alice: 'All good?', bob: 'Well, I want to talk to you about something', isComplete: true });
+    this.messages.push({ alice: 'go ahead..', bob: '', isComplete: false });
+
+  }
 
   public ngOnInit(): void {
     this.inputTextElementref.nativeElement.focus();
@@ -41,12 +59,14 @@ export class AppComponent implements OnInit {
       m.bob = this.inputText;
       m.isComplete = true;
     }
-    console.log(this.inputText);
     this.clearTextBox();
+    this.highlightLatestMessage = true;
+    setTimeout(() => {
+      this.highlightLatestMessage = false;
+    },100);
   }
 
   private clearTextBox() {
     this.inputTextElementref.nativeElement.value = '';
-    console.log(this.inputTextElementref.nativeElement);
   }
 }
--- src/app/message.ts
+++ src/app/message.ts
@@ -1,29 +1,5 @@
 export class Message {
-    
-    private _alice : string;
-    public get alice() : string {
-        return this._alice;
-    }
-    public set alice(v : string) {
-        this._alice = v;
-    }
-
-    
-    private _bob : string;
-    public get bob() : string {
-        return this._bob;
-    }
-    public set bob(v : string) {
-        this._bob = v;
-    }
-
-    
-    private _isComplete : boolean;
-    public get isComplete() : boolean {
-        return this._isComplete;
-    }
-    public set isComplete(v : boolean) {
-        this._isComplete = v;
-    }
-    
+    public alice: string;
+    public bob: string;
+    public isComplete: boolean;
 }
